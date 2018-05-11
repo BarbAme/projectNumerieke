b76e39b81214e4fefc2a96d774247d3b63c15faf
BarbAme <barbara.ameloot@student.kuleuven.be>
2018-05-11 17:42:15 +0200

    Vraag 1.3.2
    Code voor vraag 1.3.2

--- hoofdprogramma.m
+++ hoofdprogramma.m
@@ -3,6 +3,14 @@
 [x, L_sub, U_hoofd, U_super, y] = gauss_tridiag(A_sub, A_hoofd, A_super, b);
 
 %%
+%Vraag 1.3.2
+A_sub = [12;9;65];
+A_hoofd = [5;36;2;11];
+A_super = [15;17;31];
+b = [4;5;6;7];
+[x, L_sub, U_hoofd, U_super, y] = gauss_tridiag(A_sub, A_hoofd, A_super, b);
+
+%%
 %Opdracht 2.1
 [result] = evalBspline(index,k,t,x);
 
