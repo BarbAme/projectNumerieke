1d924d25ff09aa59df256ae225b069df68151cac
BarbAme <barbara.ameloot@student.kuleuven.be>
2018-05-11 17:58:13 +0200

    Opdracht 1.3
    gauss_tridiag voorzien van commentaar

--- gauss_tridiag.m
+++ gauss_tridiag.m
@@ -1,31 +1,33 @@
 function [x, L_sub, U_hoofd, U_super, y] = gauss_tridiag(A_sub, A_hoofd, A_super, b)
 
+%n = dimensie van de viekante matrix A = dimensie b
 n = length(A_hoofd);
 
+%eerste element U_hoofd
 U_hoofd(1) = A_hoofd(1);
-
+%eerste element y
 y(1) = b(1);
+
+%voorwaartse substitutie om L_sub, U_hoofd en y berekenen (en U_super overschrijven)
 for k = 2:n
+    %sub diagonaal van L
     L_sub(k-1) = A_sub(k-1)/U_hoofd(k-1);
+    
+    %hoofddiagonaal van U
     U_hoofd(k) = A_hoofd(k)-(L_sub(k-1)*A_super(k-1));
+    
+    %y
     y(k) = b(k) - (L_sub(k-1)*y(k-1));
+    
+    %superdiagonaal van U = superdiagonaal van A
     U_super(k-1) = A_super(k-1);
 end
 
+%laatste element x
 x(n) = y(n)/U_hoofd(n);
+
+%achterwaartse substitutie om x te berekenen
 for k =n-1:-1:1
     x(k) = (y(k) - A_super(k)*x(k+1))/U_hoofd(k);
 end
-
-%niet nodig bij uiteindelijke versie
-g=sprintf('%d ', x);
-fprintf('Answer x: %s\n', g)
-q=sprintf('%d ', L_sub);
-fprintf('Answer L_sub: %s\n', q)
-p=sprintf('%d ', U_hoofd);
-fprintf('Answer U_hoofd: %s\n', p)
-a=sprintf('%d ', U_super);
-fprintf('Answer U_super: %s\n', a)
-c=sprintf('%d ', y);
-fprintf('Answer y: %s\n', c)
 end
