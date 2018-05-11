b54f583cd86af97a28c1439c584356e0a9f2baa5
BarbAme <barbara.ameloot@student.kuleuven.be>
2018-05-11 15:34:52 +0200

    Debug, set_t, start 2.4
    Aantal foutjes uit vorige functies
    aparte functie om t aan te maken
    Begin van 2.4

--- hoofdprogramma.m
+++ hoofdprogramma.m
@@ -1,13 +1,20 @@
-%%Opdracht 1.3
+%%
+%Opdracht 1.3
 [x, L_sub, U_hoofd, U_super, y] = gauss_tridiag(A_sub, A_hoofd, A_super, b);
 
-%%Opdracht 2.1
+%%
+%Opdracht 2.1
 [result] = evalBspline(index,k,t,x);
 
-%%Opdracht 2.2
+%%
+%Opdracht 2.2
 [result] = evalDerivativeBspline(index,k,t,x,m);
 
-%%Opdracht 2.3
+%%
+%Opdracht 2.3
 [c] = spline_coeff(x,f);
 
+%%
+%Opdracht 2.4
+[s] = deBoor( t, c, x )
 
