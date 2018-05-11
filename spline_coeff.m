d7fc89ab75c2cca5061d75ba9f1adde99435f147
BarbAme <barbara.ameloot@student.kuleuven.be>
2018-05-11 15:58:33 +0200

    2.4, GRAFIEK NIET JUIST
    ergens een fout, vermoedelijk bij spline_coeff

--- spline_coeff.m
+++ spline_coeff.m
@@ -3,23 +3,15 @@ function [c] = spline_coeff(x,f)
    m = n+2; %dimensie matrix
    A = zeros(m,m);
    B = zeros(m,1);
-
    t = set_t(x);
 
    %MATRIX A
-
-   
    %matrix A, rij 1, kolom 1,2,3
-   
    for j = 1:3
        index= -4+j;
        A(1,j) = evalDerivativeBspline(index+3,3,t,x(0+1),2);
    end
-
-   
    %matrix A, rij 2 tem voorlaatste
-   
-
    for i = 2:n+1
        for j = i:i+2 % de 3 in te vullen kolommen
            index = -4+j;
@@ -27,15 +19,11 @@ function [c] = spline_coeff(x,f)
            A(i,j-1) = evalBspline(index+2,3,t,x(i-1));
        end
    end
-
-   %matrix A, laatste rij
-   
+   %matrix A, laatste rij 
    for j = n+1:n+3
        index = -4+j;
-
        A(n+2,j-1) = evalDerivativeBspline(index+2,3,t,x(n),2);
    end
-
    
   % MATRIX B
    %eerste en laatste rij moeten 0 blijven
